f7d2d5ca144944e74918a8d905a0d2e747ecaba4
pr3mar <n1A@s3A2>
2015-04-09 23:43:30 +0200

    Home work 3, questions 1.a and 1.b

---
+++ prDN3/src/task3_start.py
@@ -0,0 +1,121 @@
+try:
+    import sys
+    sys.path.remove(filter(lambda e: "orange3" in e, sys.path)[0])
+except:
+    pass
+
+import Orange
+import numpy as np
+
+### Use as desired:
+import Orange
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy.cluster.hierarchy import linkage, dendrogram
+from itertools import combinations
+
+
+def count(data, value):
+    c = 0
+    for i in data:
+        if(data[i] == value):
+            c += 1
+    return c
+
+# Load data
+Utab = Orange.data.Table("../data/user.tab")
+Itab = Orange.data.Table("../data/item.tab")
+Dtab = Orange.data.Table("../data/data.tab")
+Ctab = Orange.data.Table("../data/cast.tab")
+
+U, _, _ = Utab.to_numpy()
+I, _, _ = Itab.to_numpy()
+D, _, _ = Dtab.to_numpy()
+C, _, _ = Ctab.to_numpy()
+
+# Movies per genre
+print Itab.domain
+
+MPG = I.sum(axis=0).astype(int)
+print "Movies per genre:", MPG
+
+
+# Select genres with at least T movies
+T = 50
+inxs = map(int, np.where(I.sum(axis=0) >= T)[0])
+Itab.domain = [Itab.domain[i] for i in inxs]
+I = I[:, inxs]
+MPG = MPG[inxs]
+print "Filtered genres", Itab.domain
+for i in range(len(Itab.domain)):
+    print i, Itab.domain[i].name
+
+
+###########
+#  PART 1 #
+###########
+
+
+# Prepare data and impute
+# VPG: Views per genre
+# UG: User genre rating
+# Also, Do not account for averages with less than 5 samples
+VPG = (D > 0).dot(I)
+UG = np.nan_to_num(D.dot(I) / VPG)
+
+for r, c in zip(*((VPG < 5).nonzero())):
+    inxs = (VPG[:, c] >= 5).nonzero()[0]
+    UG[r, c] = np.mean(UG[inxs, c])
+
+# Normalize UG to the average of each users respective rates
+UG = UG / UG.mean(axis=1).reshape((len(UG), 1))
+
+# YOUR CODE HERE
+
+# 1.a code
+k = 4
+model = KMeans(n_clusters=k)
+y = model.fit_predict(UG)
+appearance = np.bincount(y)
+# question 1.a answer
+print appearance
+# 1.b code
+cluster_indices = []
+for i in range(k):
+    cluster_indices.append(np.where(y == i)[0])
+avg_by_genre = UG.mean(axis=0)
+# print MPG.shape
+# print avg_by_genre
+# print UG.shape
+avg_per_cluster = []
+for i in range(k):
+    avg_per_cluster.append(UG[cluster_indices[i],:].mean(axis=0))
+avg_all = []
+for i in avg_per_cluster:
+    avg_all.append(i/avg_by_genre)
+# print the max values
+# question 1.b answer
+for i in avg_all:
+    print max(i), i.argmin()
+
+#avg_per_genre = D.sum(axis=1).astype(int) / MPG
+#print avg_per_genre
+
+
+# ###########
+# #  PART 2 #
+# ###########
+#
+# # Select movies with at least 250 views
+# # Their indices are in inxs
+# T = 250
+# inxs = map(int, np.where((D > 0).sum(axis=0) > T)[0])
+# n = len(inxs)
+# print "Number of movies: ", n
+#
+# # YOUR CODE HERE
+
+
+
+
+

3dc1c5701e2bdfae45bfbffcc9d6dcf6bea6ec01
pr3mar <n1A@s3A2>
2015-05-07 13:13:38 +0200

    dn4 1 a)

--- prDN4/src/task4_starter.py
+++ prDN4/src/task4_starter.py
@@ -59,60 +59,69 @@ class Majority:
 # Sample code: PART 1
 # Prediction of whether the user likes a movie or not (class), based on all other users
 #   All other users are now considered as 'attributes' of the classification problem.
-i = 302     # Randomly choose a user on index 302
-X = np.hstack([D[:i, :].T, D[i+1:, :].T])   # Attributes are all other users
-y = np.hstack([D[i, :]])  # Class labels are movie ratings of user 301
-index_set = np.where(y > 0)[0]  # Select only the movies that user watched
-X = X[index_set, :]     # Training data - attributes
-y = y[index_set]     # Training data - class labels
-
-# Set threshold to convert rating to two classes;
-# Ratings > tau are considered as "Like" (class 1) and otherwise as "Unlike" (class 0)
-tau = 2
-y = y > tau
-
-# Sample usage of models
-model = Majority()
-model.fit(X, y)                 # Train model on all data
-prediction = model.predict(X)   # Predict the same training data
-P = precision_score(y, prediction)   # Measure precision score
-print "Precision of the majority vote: %f" % P
-
-
-
-
-#######################
-# Sample code: PART 2 #
-#######################
-# Classification of users; separation between occupations.
-# How well can we separate eg. scientists and salesmen based on movie ratings?
-# Build classifier for each pair of jobs
-# Exclude 'none' and 'other'
-jobs = sorted(list(set([str(u["occupation"]) for u in Utab])))
-jobs.remove("none")
-jobs.remove("other")
-
-j1 = "scientist"
-j2 = "salesman"
-inxs1 = map(lambda e: e[0], filter(lambda e: str(e[1]["occupation"]) == j1, enumerate(Utab)))
-inxs2 = map(lambda e: e[0], filter(lambda e: str(e[1]["occupation"]) == j2, enumerate(Utab)))
-X = D[inxs1+inxs2, :]                                # Select appropriate indices
-y = np.array(len(inxs1) * [0] + len(inxs2) * [1])    # Assign users to two classes
-
-# Select top 200 viewed movies for the pair of users' types
-top_movie_inxs = np.argsort((X > 0).sum(axis=0))[::-1][:200]
-X = X[:, top_movie_inxs]
+i = 0     # Randomly choose a user on index 302
+tau = [2, 3, 4]
+for t in tau:
+    for i in range(U.shape[0]):
+        X = np.hstack([D[:i, :].T, D[i+1:, :].T])   # Attributes are all other users
+        y = np.hstack([D[i, :]])  # Class labels are movie ratings of user 301
+        index_set = np.where(y > 0)[0]  # Select only the movies that user watched
+        X = X[index_set, :]     # Training data - attributes
+        y = y[index_set]     # Training data - class labels
+        try:
+            split = StratifiedShuffleSplit(y, test_size=0.1)
+            # Set threshold to convert rating to two classes;
+            # Ratings > tau are considered as "Like" (class 1) and otherwise as "Unlike" (class 0)
+            y = y > t
+            for train_indice, test_indice in split:
+                # Sample usage of models
+                train_set = X[train_indice, :]
+                train_set_labels = y[train_indice]
+                test_set = X[test_indice, :]
+                test_set_labels = y[test_indice]
+                model = Majority()
+                model.fit(train_set, train_set_labels)                 # Train model on all data
+                prediction = model.predict(test_set)   # Predict the same training data
+                precision = precision_score(test_set_labels, prediction)   # Measure precision score
+                recall = recall_score(test_set_labels, prediction)   # Measure precision score
+                area = roc_auc_score(test_set_labels, prediction)   # Measure precision score
+                print "[t = %i] precision: %f, recall: %f, area: %f" % (t, precision, recall, area)
+        except:
+            pass
 
 
-# Train a decision tree and export tree graph in .dot format
-# To draw, install
-#       http://www.graphviz.org/
-#   and run:
-#       dot -Tpdf jobs.dot -o graph.pdf
-model = DecisionTreeClassifier()
-model.fit(X, y)
-with open("jobs.dot", 'w') as f:
-    export_graphviz(model, out_file=f, feature_names=map(str, [Dtab.domain.attributes[mi].name for mi in top_movie_inxs]))
+# #######################
+# # Sample code: PART 2 #
+# #######################
+# # Classification of users; separation between occupations.
+# # How well can we separate eg. scientists and salesmen based on movie ratings?
+# # Build classifier for each pair of jobs
+# # Exclude 'none' and 'other'
+# jobs = sorted(list(set([str(u["occupation"]) for u in Utab])))
+# jobs.remove("none")
+# jobs.remove("other")
+#
+# j1 = "scientist"
+# j2 = "salesman"
+# inxs1 = map(lambda e: e[0], filter(lambda e: str(e[1]["occupation"]) == j1, enumerate(Utab)))
+# inxs2 = map(lambda e: e[0], filter(lambda e: str(e[1]["occupation"]) == j2, enumerate(Utab)))
+# X = D[inxs1+inxs2, :]                                # Select appropriate indices
+# y = np.array(len(inxs1) * [0] + len(inxs2) * [1])    # Assign users to two classes
+#
+# # Select top 200 viewed movies for the pair of users' types
+# top_movie_inxs = np.argsort((X > 0).sum(axis=0))[::-1][:200]
+# X = X[:, top_movie_inxs]
+#
+#
+# # Train a decision tree and export tree graph in .dot format
+# # To draw, install
+# #       http://www.graphviz.org/
+# #   and run:
+# #       dot -Tpdf jobs.dot -o graph.pdf
+# model = DecisionTreeClassifier()
+# model.fit(X, y)
+# with open("jobs.dot", 'w') as f:
+#     export_graphviz(model, out_file=f, feature_names=map(str, [Dtab.domain.attributes[mi].name for mi in top_movie_inxs]))
 
 
 
